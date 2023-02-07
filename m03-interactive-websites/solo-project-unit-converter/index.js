a76200542b05710535440cd7cf431cf4b1a07a45
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-08 00:53:37 +0545

    start: solo project unit converter

---
+++ m03-interactive-websites/solo-project-unit-converter/index.js
@@ -0,0 +1,9 @@
+/*
+Module 3 - Solo Project Unit Converter
+Project details: https://scrimba.com/learn/frontend/solo-project-unit-converter-cz9aPNSr
+Figma file: https://www.figma.com/file/cqtGul0V8RFXY4vTcIv1Kc/Unit-Conversion?node-id=0%3A1&t=gcrunYDTerV6n3lC-0
+
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
