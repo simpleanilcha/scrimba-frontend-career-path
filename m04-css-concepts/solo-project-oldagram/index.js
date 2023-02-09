73b96bf8997daa378eaab00b82f687b0456f5b51
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-09 23:32:21 +0545

    Initial setup: solo project oldagram

---
+++ m04-css-concepts/solo-project-oldagram/index.js
@@ -0,0 +1,40 @@
+// Module 4 - Solo Project Unit Converter
+// Project details: https://scrimba.com/learn/frontend/solo-project-oldagram-co2274297820a9405442e3a2a
+// Figma file: https://www.figma.com/file/h0MKma9TTWzGOMQ9Ia6ROW/Oldagram?node-id=0%3A1&t=40iJISTKNJOnFtNu-0
+
+// Solution:
+// link: 
+// scrim: 
+
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+
+
