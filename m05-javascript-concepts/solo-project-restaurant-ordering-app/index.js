1c1585958159c458a4db9e180d6795e8e24bb387
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-15 01:14:44 +0545

    start: solo project - restaurant ordering app

---
+++ m05-javascript-concepts/solo-project-restaurant-ordering-app/index.js
@@ -0,0 +1,13 @@
+// Module 5 - Solo Project Restaurant Ordering app
+// Project details: https://scrimba.com/learn/frontend/solo-project-restaurant-ordering-app-co72e499baf5f48346e5975cf
+// https://scrimba.com/links/solo-project-restaurant-menu-figma
+// Figma file: https://www.figma.com/file/Hdgwo69Dym9vVsxbuPbl0h/Mobile-Restaurant-Menu?node-id=0%3A1&t=IpzmpgUZ25nw8gpm-0
+
+// help-section: https://projects.scrimba.com/
+
+// Solution:
+// link: 
+// scrim: 
+
+import { menuArray } from './data.js'
+console.log(menuArray)
