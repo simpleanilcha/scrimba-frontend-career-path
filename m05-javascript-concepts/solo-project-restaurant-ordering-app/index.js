1e672de9cad2a6178f1465a2dd104c61f961c424
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-16 18:55:17 +0545

    update: add orders

--- m05-javascript-concepts/solo-project-restaurant-ordering-app/index.js
+++ m05-javascript-concepts/solo-project-restaurant-ordering-app/index.js
@@ -11,7 +11,6 @@
 
 import { menuArray } from './data.js'
 
-const orderBtn = document.getElementById('order-btn')
 const payModal = document.querySelector('#modal')
 const bodyEl = document.querySelector('body')
 let isModalOpen = false
@@ -26,15 +25,15 @@ menuArray.forEach(menu => {
       <div class="order">
         <div class="order-item">
           <!--<img src="./images/pizza.jpg" alt="pizza slice"> -->
-          ${menu.emoji}
+          <p class="item-emoji">${menu.emoji}</p>
         </div>
         <div class="order-details">
           <h2>${menu.name}</h2>
-          <p>pepperoni,mushrom,mozarella</p>
+          <p>${menu.ingredients.toString()}</p>
           <p class="order-price">$${menu.price}</p>
         </div>
         <div class="order-add">
-          <button>
+          <button class="btn-add">
             <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M6.83949 13.8068V0.011363H8.16903V13.8068H6.83949ZM0.612216 7.57955V6.23864H14.3963V7.57955H0.612216Z" fill="#3C3C3C"/>
               </svg>
@@ -48,8 +47,49 @@ menuArray.forEach(menu => {
 // mainEl.innerHTML = menuList
 orderMenuEl.innerHTML = menuList
 
+const addBtns = document.querySelectorAll('.btn-add')
+console.log(addBtns)
+let orderCount = 0
+const orderEl = document.querySelector('#complete-order .container')
+let totalPrice = 0
+
+let orderItem = ''
+
+addBtns.forEach((btn, index) => {
+  btn.addEventListener('click', (e) => {
+    
+    orderCount++
+    let clickedEl = menuArray[index]
+
+    totalPrice += clickedEl.price
+
+    orderItem += `
+      <div class="order-item">
+        <h2>${clickedEl.name} <span>remove</span></h2>
+        <p>$${clickedEl.price}</p>
+      </div>
+    `
+
+    orderEl.innerHTML = `
+      <h2>Your order</h2>
+      <div class="order-items">
+        ${orderItem}
+        <div class="order-item total-price">
+          <h2>Total price:</h2>
+          <p>$${totalPrice}</p>
+        </div>
+      </div>
+      <button class="btn btn-normal" id="order-btn">Complet order</button>
+    `
+    console.log(clickedEl)
+    checkOrderBtnClick()
+  })
+})
+
+
 // MODAL OPEN CLOSE
 function checkOrderBtnClick() {
+  const orderBtn = document.getElementById('order-btn')
   if (!isModalOpen) {
     orderBtn.addEventListener('click', () => {
       payModal.classList.remove('d-none')
@@ -81,4 +121,4 @@ function closeModal (event) {
   }
 }
 
-checkOrderBtnClick()
+// checkOrderBtnClick()
