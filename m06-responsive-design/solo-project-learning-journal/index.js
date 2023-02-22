a4791611c4bf2896254e6e166471a2bb48faafe1
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-22 11:37:04 +0545

    setup: solo project learning journal

---
+++ m06-responsive-design/solo-project-learning-journal/index.js
@@ -0,0 +1,7 @@
+// Module 6 - Solo Project: Learning Journal
+// Project details: https://scrimba.com/learn/frontend/solo-project-learning-journal-cNP6Gwc6
+// Figma file: https://www.figma.com/file/hE5klIn1AEQ9XWZWmurs7y/Learning-Journal%2FBlog?node-id=0%3A1&t=ub6CGh4ikvPXJNKi-0
+
+// Solution:
+// link: 
+// scrim: 
