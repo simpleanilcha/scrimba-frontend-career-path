60efc35f328d7285d81a4fddcdf7cd5fbf2dc3ce
Anil Maharjan <simpleanilcha@gmail.com>
2023-04-11 07:38:11 +0545

    pass props to card component

--- m11-react-basics/airbnb-clone/index.pack.js
+++ m11-react-basics/airbnb-clone/index.pack.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 9);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -264,9 +264,9 @@ process.umask = function() { return 0; };
 /* WEBPACK VAR INJECTION */(function(process) {
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = __webpack_require__(11);
+  module.exports = __webpack_require__(13);
 } else {
-  module.exports = __webpack_require__(10);
+  module.exports = __webpack_require__(12);
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
@@ -376,9 +376,9 @@ module.exports = shouldUseNative() ? Object.assign : function (target, source) {
 /* WEBPACK VAR INJECTION */(function(process) {
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = __webpack_require__(15);
+  module.exports = __webpack_require__(17);
 } else {
-  module.exports = __webpack_require__(14);
+  module.exports = __webpack_require__(16);
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
@@ -399,15 +399,15 @@ var _react = __webpack_require__(1);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _Navbar = __webpack_require__(6);
+var _Navbar = __webpack_require__(8);
 
 var _Navbar2 = _interopRequireDefault(_Navbar);
 
-var _Hero = __webpack_require__(17);
+var _Hero = __webpack_require__(7);
 
 var _Hero2 = _interopRequireDefault(_Hero);
 
-var _Card = __webpack_require__(18);
+var _Card = __webpack_require__(6);
 
 var _Card2 = _interopRequireDefault(_Card);
 
@@ -419,7 +419,14 @@ function App() {
         null,
         _react2.default.createElement(_Navbar2.default, null),
         _react2.default.createElement(_Hero2.default, null),
-        _react2.default.createElement(_Card2.default, null)
+        _react2.default.createElement(_Card2.default, {
+            imgPath: "katie-zaferes.png",
+            rating: "5.0",
+            reviewCount: 6,
+            country: "USA",
+            title: "Life Lessons with Katie Zaferes",
+            price: 136
+        })
     );
 }
 
@@ -462,15 +469,137 @@ if (process.env.NODE_ENV === 'production') {
   // DCE check should happen before ReactDOM bundle executes so that
   // DevTools can report bad minification during injection.
   checkDCE();
-  module.exports = __webpack_require__(9);
+  module.exports = __webpack_require__(11);
 } else {
-  module.exports = __webpack_require__(8);
+  module.exports = __webpack_require__(10);
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
 /* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Card;
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/*
+Challenge: Build the Card component
+For now, hard-code in the data (like 
+the rating, title, price, etc.)
+
+Notes:
+- Only render 1 instance (I already did this for you)
+- The star icon and photo (katie-zaferes.png) are in the images 
+  folder for your use
+- Make sure to include:
+    - image
+    - star icon (star.png), rating, and review count
+    - title
+    - cost/person
+- The main purpose of this challenge is to show you where our limitations
+  currently are, so don't worry about the fact that you're hard-coding all
+  this data into the component.
+*/
+
+function Card(props) {
+  return _react2.default.createElement(
+    "div",
+    { className: "card" },
+    _react2.default.createElement("img", {
+      src: "../images/" + props.imgPath,
+      className: "card--image " + (props.imgPath ? 'x' : 'y')
+    }),
+    _react2.default.createElement(
+      "div",
+      { className: "card--stats" },
+      _react2.default.createElement("img", { src: "../images/star.png", className: "card--star" }),
+      _react2.default.createElement(
+        "span",
+        null,
+        props.rating
+      ),
+      _react2.default.createElement(
+        "span",
+        { className: "gray" },
+        "(",
+        props.reviewCount,
+        ") \u2022 "
+      ),
+      _react2.default.createElement(
+        "span",
+        { className: "gray" },
+        props.country
+      )
+    ),
+    _react2.default.createElement(
+      "p",
+      null,
+      props.title
+    ),
+    _react2.default.createElement(
+      "p",
+      null,
+      _react2.default.createElement(
+        "span",
+        { className: "bold" },
+        "From $",
+        props.price
+      ),
+      " / person"
+    )
+  );
+}
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Hero;
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Hero() {
+  return _react2.default.createElement(
+    "section",
+    { className: "hero" },
+    _react2.default.createElement("img", { src: "../images/photo-grid.png", className: "hero--photo" }),
+    _react2.default.createElement(
+      "h1",
+      { className: "hero--header" },
+      "Online Experiences"
+    ),
+    _react2.default.createElement(
+      "p",
+      { className: "hero--text" },
+      "Join unique interactive activities led by one-of-a-kind hosts\u2014all without leaving home."
+    )
+  );
+}
+
+/***/ }),
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -496,7 +625,7 @@ function Navbar() {
 }
 
 /***/ }),
-/* 7 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -519,7 +648,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 _reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById("root"));
 
 /***/ }),
-/* 8 */
+/* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -541,7 +670,7 @@ if (process.env.NODE_ENV !== "production") {
 var React = __webpack_require__(1);
 var _assign = __webpack_require__(2);
 var Scheduler = __webpack_require__(3);
-var tracing = __webpack_require__(16);
+var tracing = __webpack_require__(18);
 
 var ReactSharedInternals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
@@ -26789,7 +26918,7 @@ exports.version = ReactVersion;
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 9 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -27093,7 +27222,7 @@ exports.unstable_renderSubtreeIntoContainer=function(a,b,c,d){if(!rk(c))throw Er
 
 
 /***/ }),
-/* 10 */
+/* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -29434,7 +29563,7 @@ exports.version = ReactVersion;
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 11 */
+/* 13 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -29464,7 +29593,7 @@ exports.useLayoutEffect=function(a,b){return S().useLayoutEffect(a,b)};exports.u
 
 
 /***/ }),
-/* 12 */
+/* 14 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -29819,7 +29948,7 @@ exports.unstable_wrap = unstable_wrap;
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 13 */
+/* 15 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -29835,7 +29964,7 @@ var b=0;exports.__interactionsRef=null;exports.__subscriberRef=null;exports.unst
 
 
 /***/ }),
-/* 14 */
+/* 16 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -30489,7 +30618,7 @@ exports.unstable_wrapCallback = unstable_wrapCallback;
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 15 */
+/* 17 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -30516,135 +30645,19 @@ exports.unstable_wrapCallback=function(a){var b=P;return function(){var c=P;P=b;
 
 
 /***/ }),
-/* 16 */
+/* 18 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 /* WEBPACK VAR INJECTION */(function(process) {
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = __webpack_require__(13);
+  module.exports = __webpack_require__(15);
 } else {
-  module.exports = __webpack_require__(12);
+  module.exports = __webpack_require__(14);
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
-
-/***/ }),
-/* 17 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = Hero;
-
-var _react = __webpack_require__(1);
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function Hero() {
-  return _react2.default.createElement(
-    "section",
-    { className: "hero" },
-    _react2.default.createElement("img", { src: "../images/photo-grid.png", className: "hero--photo" }),
-    _react2.default.createElement(
-      "h1",
-      { className: "hero--header" },
-      "Online Experiences"
-    ),
-    _react2.default.createElement(
-      "p",
-      { className: "hero--text" },
-      "Join unique interactive activities led by one-of-a-kind hosts\u2014all without leaving home."
-    )
-  );
-}
-
-/***/ }),
-/* 18 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = Card;
-
-var _react = __webpack_require__(1);
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-function Card() {
-  return _react2.default.createElement(
-    "div",
-    { className: "card" },
-    _react2.default.createElement("img", { src: "../images/katie-zaferes.png", className: "card--image" }),
-    _react2.default.createElement(
-      "div",
-      { className: "card--stats" },
-      _react2.default.createElement("img", { src: "../images/star.png", className: "card--star" }),
-      _react2.default.createElement(
-        "span",
-        null,
-        "5.0"
-      ),
-      _react2.default.createElement(
-        "span",
-        { className: "gray" },
-        "(6) \u2022 "
-      ),
-      _react2.default.createElement(
-        "span",
-        { className: "gray" },
-        "USA"
-      )
-    ),
-    _react2.default.createElement(
-      "p",
-      null,
-      "Life Lessons with Katie Zaferes"
-    ),
-    _react2.default.createElement(
-      "p",
-      null,
-      _react2.default.createElement(
-        "span",
-        { className: "bold" },
-        "From $136"
-      ),
-      " / person"
-    )
-  );
-}
 
 /***/ })
 /******/ ]);
